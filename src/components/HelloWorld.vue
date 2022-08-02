f4fa8a43c6cf27ccf88696d9395cb411de161894
talel <talelmejri8@gmail.com>
2022-08-02 13:27:55 +0100

    fiw row,col in home page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,25 +2,25 @@
 <div class="hello">
     <section class="py-5 ">
             <div class="container px-lg-5 mt-5">
-              <div class="hero">
-                <div class="static_text">Welcome </div>
-                    <ul class="dynamique">
+              <div class="hero text-center row">
+                <div id="welcome" class="static_text col-lg-12 fw-bolder">Welcome </div>
+                    <ul class="dynamique col-lg-6">
                         <li><span>To</span></li>
                         <li><span>Our</span></li>
                         <li><span>New</span></li>
                         <li><span>Website</span></li>
                     </ul>
             </div>
-                <div class="row py-5">
+                <div class="row py-5 gap-5">
                        <div class="col-md-4 text-center py-4">
                             <img src="../assets/Motivation4.jpg" alt="" class="shadow rounded-pill" width="300px" height="300px">
                        </div>
-                       <div class="col-md-4 text-center py-5 ">
+                       <div class="col-lg-4 text-center py-5 ">
                            <p style="font-size:22px" class="py-5 fw-bolder">
                                  " Success is the most beautiful thing in the world. "
                            </p>
                        </div>
-                        <div class="col-md-3 text-center py-4">
+                        <div class="col-md-2 text-center py-4">
                             <img src="../assets/Best-success-quotes-pics-status-thought-12.jpg" class="shadow rounded-pill" alt="" width="200px" height="300px">
                        </div>
                 </div>
@@ -38,7 +38,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-p{
+p,#welcome{
     text-shadow: 5px 5px 5px #403c32;
 }
 .hero{
