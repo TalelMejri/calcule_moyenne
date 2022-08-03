be9ee3145ae9546bc76d0bbeeeea8f4700e39b36
talel <talelmejri8@gmail.com>
2022-08-03 11:12:20 +0100

    add spinner

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <!-- <vue-simple-spinner class="display-2 mt-5 py-5" size="large" />-->
     <App_Header></App_Header>
     <router-view/>
     <App_footer></App_footer>
@@ -9,11 +10,12 @@
 <script>
 import App_Header from "./components/App_Header.vue";
 import App_footer from "./components/App_footer.vue";
-
+//import VueSimpleSpinner from 'vue-simple-spinner';
  export default {
     name:'app',
     components:{
        App_Header,App_footer
+       //,VueSimpleSpinner
     }
  }
 
